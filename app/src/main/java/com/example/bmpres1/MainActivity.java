d1301165da4da7fb5d458125f487f08ed3f34e33
Lukasz Kuk <lukasz.kuk@motorolasolutions.com>
2021-05-19 14:50:23 +0200

    added negative

--- app/src/main/java/com/example/bmpres1/MainActivity.java
+++ app/src/main/java/com/example/bmpres1/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.bmpres1;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private Bitmap bitmapCircles;
 
     private static final int LENGTH = 8;
-    private static final int INCREASE = 1;
+    private static final int MAX_RANDOM = 2000;
     private static final Paint.Style STYLE = Paint.Style.FILL;
 
     @Override
@@ -58,32 +61,62 @@ public class MainActivity extends AppCompatActivity {
         final int length = LENGTH / 2;
         Canvas canvas = new Canvas(bitmapSharp);
         Paint paint = new Paint();
+        Random random = new Random();
 
-        for (int i = 0; i < bitmapSharp.getWidth(); i += INCREASE) {
-            for (int j = 0; j < bitmapSharp.getHeight(); j += INCREASE) {
-                int color = bitmapSharp.getPixel(i, j);
+        for (int i = 0; i < MAX_RANDOM; i++) {
 
-                paint.setColor(color);
-                paint.setStyle(STYLE);
+            int x = random.nextInt(bitmapSharp.getWidth());
+            int y = random.nextInt(bitmapSharp.getHeight());
+            int color = bitmapSharp.getPixel(x, y);
 
-                canvas.drawLine(i - length, j - length, i + length, j + length, paint);
-                canvas.drawLine(i - length, j + length, i + length, j - length, paint);
-            }
+            paint.setColor(color);
+            paint.setStyle(STYLE);
+
+            canvas.drawLine(x - length, y - length, x + length, y + length, paint);
+            canvas.drawLine(x - length, y + length, x + length, y - length, paint);
         }
     }
 
     private void paintCircles() {
         Canvas canvas = new Canvas(bitmapCircles);
         Paint paint = new Paint();
+        Random random = new Random();
 
-        for (int i = 0; i < bitmapCircles.getWidth(); i += INCREASE) {
-            for (int j = 0; j < bitmapCircles.getHeight(); j += INCREASE) {
-                int color = bitmapCircles.getPixel(i, j);
+        for (int i = 0; i < MAX_RANDOM; i++) {
 
-                paint.setColor(color);
-                paint.setStyle(STYLE);
+            int x = random.nextInt(bitmapCircles.getWidth());
+            int y = random.nextInt(bitmapCircles.getHeight());
+            int color = bitmapCircles.getPixel(x, y);
 
-                canvas.drawCircle(i, j, LENGTH, paint);
+            paint.setColor(color);
+            paint.setStyle(STYLE);
+
+            canvas.drawCircle(x, y, LENGTH, paint);
+        }
+    }
+
+    public void onBitmap3(View view) {
+        paintNegative();
+
+        imageSharp.setImageBitmap(bitmapSharp);
+        textView.setText(String.format(getString(R.string.IMG_INFO),
+                imageSharp.getWidth(), imageSharp.getHeight(),
+                bitmapSharp.getWidth(), bitmapSharp.getHeight()));
+    }
+
+    private void paintNegative() {
+        int bitmapWidth = bitmapSharp.getWidth();
+        int bitmapHeight = bitmapSharp.getHeight();
+
+        for (int i = 0; i < bitmapWidth; i++) {
+            for (int j = 0; j < bitmapHeight; j++) {
+
+                int color = bitmapSharp.getPixel(i, j);
+                int r = Color.red(color);
+                int g = Color.green(color);
+                int b = Color.blue(color);
+
+                bitmapSharp.setPixel(i, j, Color.rgb(255 - r, 255 - g, 255 - b));
             }
         }
     }
